dde50ab29d786be065183e6dce1fac313dc29b2d
vigovlugt <vigovlugt@gmail.com>
2026-05-30 10:54:14 +0200

    Add flake and fix partially nix build (still crashing tho)

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
     },
     server: {
         port: 3000,
+        watch: {
+            ignored: ['**/src-tauri/target/**'],
+        },
     },
     build: {
         target: "esnext",
